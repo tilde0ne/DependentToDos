e65c16fedb3914f0d8a3d9fc6057154c3e2508ff
tilde0ne <miyako@freenet.de>
2018-09-10 19:37:38 +0200

    Added time field  (+Getter&Setter) and serialVersionUID to Appointment.

--- src/name/fmader/datamodel/Appointment.java
+++ src/name/fmader/datamodel/Appointment.java
@@ -1,8 +1,15 @@
 package name.fmader.datamodel;
 
+import javafx.beans.property.SimpleObjectProperty;
+
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Appointment extends ToDoItem {
+
+    private static final long serialVersionUID = -5873691858014094083L;
+
+    private SimpleObjectProperty<LocalTime> time;
 
     public Appointment(String title) {
         super(title);
@@ -22,10 +29,23 @@ public class Appointment extends ToDoItem {
     public void setStart(LocalDate start) {
     }
 
+    public LocalTime getTime() {
+        return time.get();
+    }
+
+    public SimpleObjectProperty<LocalTime> timeProperty() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time.set(time);
+    }
+
     @Override
     public String toString() {
         return "Appointment{" +
-                "title=" + title +
+                "time=" + time +
+                ", title=" + title +
                 ", description='" + description + '\'' +
                 ", deadline=" + deadline +
                 ", start=" + start +
