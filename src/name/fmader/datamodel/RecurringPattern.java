37bd1a063ec0274cec520f75db875b7f297e6a22
tilde0ne <miyako@freenet.de>
2018-09-10 22:11:14 +0200

    Reformatted RecurringPattern

--- src/name/fmader/datamodel/RecurringPattern.java
+++ src/name/fmader/datamodel/RecurringPattern.java
@@ -6,19 +6,17 @@ public class RecurringPattern implements Serializable {
 
     private static final long serialVersionUID = -4442714177536551237L;
 
-    /*
-        fix:
+    /*fix:
 
-        if false (only valid if NO deadline is specified), a new item will be created when done() is called,
-        start will be as values dictate;
+    if false (only valid if NO deadline is specified), a new item will be created when done() is called,
+    start will be as values dictate;
 
-        if true AND NO deadline is specified, a new item will be created with the specified offset to when
-        current item is created (or if specified reaches start date);
+    if true AND NO deadline is specified, a new item will be created with the specified offset to when
+    current item is created (or if specified reaches start date);
 
-        if true AND a deadline is specified, a new item will be created when current item is due,
-        deadline will be as values dictate, start will be added according to the current start-due range
-        if it lies in the future at creation date;
-        */
+    if true AND a deadline is specified, a new item will be created when current item is due,
+    deadline will be as values dictate, start will be added according to the current start-due range
+    if it lies in the future at creation date;*/
     private boolean fix;
 
     private int everyNDays;
