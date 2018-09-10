c225895ebef8192e2df9807f13596fa727098ac7
tilde0ne <miyako@freenet.de>
2018-09-10 16:44:12 +0200

    Added toString to External

--- src/name/fmader/datamodel/External.java
+++ src/name/fmader/datamodel/External.java
@@ -5,4 +5,19 @@ public class External extends ToDoItem {
     public External(String title) {
         super(title);
     }
+
+    @Override
+    public String toString() {
+        return "External{" +
+                "title=" + title +
+                ", description='" + description + '\'' +
+                ", deadline=" + deadline +
+                ", start=" + start +
+                ", dependsOn=" + dependsOn +
+                ", dependedOnBy=" + dependedOnBy +
+                ", contexts=" + contexts +
+                ", isRecurrent=" + isRecurrent +
+                ", recurringPattern=" + recurringPattern +
+                '}';
+    }
 }
