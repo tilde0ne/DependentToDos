18ad8758782a575bf9ae44db0da76f5f642d3fdf
tilde0ne <miyako@freenet.de>
2018-09-10 21:27:35 +0200

    Started RecurringPatterns

--- src/name/fmader/datamodel/RecurringPattern.java
+++ src/name/fmader/datamodel/RecurringPattern.java
@@ -1,4 +1,13 @@
 package name.fmader.datamodel;
 
-public class RecurringPattern {
+import java.io.Serializable;
+
+public class RecurringPattern implements Serializable {
+
+    /*
+    fix: if false, counting starts when item's done() is called;
+    if true, counting starts when current item is created
+    */
+    private boolean fix;
+    private boolean dateDependand;
 }
