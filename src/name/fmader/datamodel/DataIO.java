f463c98a186598fd06438c3b77629584fd3c255d
tilde0ne <miyako@freenet.de>
2018-09-11 12:42:56 +0200

    Implemented load() and save() for DataIO

--- src/name/fmader/datamodel/DataIO.java
+++ src/name/fmader/datamodel/DataIO.java
@@ -1,5 +1,6 @@
 package name.fmader.datamodel;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,11 +8,11 @@ public class DataIO {
 
     public static final DataIO instance = new DataIO();
 
-    private final List<ToDoItem> toDoItems;
-    private final List<Appointment> appointments;
-    private final List<External> externals;
-    private final List<Project> projects;
-    private final List<String> contexts;
+    private List<ToDoItem> toDoItems;
+    private List<Appointment> appointments;
+    private List<External> externals;
+    private List<Project> projects;
+    private List<String> contexts;
 
     private DataIO() {
         this.toDoItems = new ArrayList<>();
@@ -22,11 +23,33 @@ public class DataIO {
     }
 
     public boolean load() {
-        return false;
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("data.dtd"))) {
+            toDoItems = (ArrayList<ToDoItem>) in.readObject();
+            appointments = (ArrayList<Appointment>) in.readObject();
+            externals = (ArrayList<External>) in.readObject();
+            projects = (ArrayList<Project>) in.readObject();
+            contexts = (ArrayList<String>) in.readObject();
+
+            return true;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public boolean save() {
-        return false;
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data.dtd"))) {
+            out.writeObject(toDoItems);
+            out.writeObject(appointments);
+            out.writeObject(externals);
+            out.writeObject(projects);
+            out.writeObject(contexts);
+
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     // Getters
