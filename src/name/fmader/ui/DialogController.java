492852903fb47280e8b2a5908ede0dc0d1a55b28
tilde0ne <miyako@freenet.de>
2018-09-14 16:48:51 +0200

    Finished DialogController.initForm() Implemented DialogController.initialize() up to: set visible/invisible everything add listeners to keep child, parent and context sources filtered

--- src/name/fmader/ui/DialogController.java
+++ src/name/fmader/ui/DialogController.java
@@ -1,17 +1,36 @@
 package name.fmader.ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import name.fmader.datamodel.Appointment;
-import name.fmader.datamodel.External;
-import name.fmader.datamodel.RecurringPattern;
-import name.fmader.datamodel.ToDoItem;
+import name.fmader.datamodel.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class DialogController {
 
     private ToDoItem selectedToDoItem = null;
+
+    private DataIO dataIO = DataIO.getInstance();
+
+    private ObservableList<ToDoItem> children = FXCollections.observableArrayList();
+    private ObservableList<ToDoItem> parents = FXCollections.observableArrayList();
+    private ObservableList<String> itemContexts = FXCollections.observableArrayList();
+    private ObservableList<ToDoItem> toDoItems = FXCollections.observableArrayList(dataIO.getToDoItems());
+    private ObservableList<String> availableContexts = FXCollections.observableArrayList(dataIO.getContexts());
+
+    private List<ListView<ToDoItem>> listViews = new ArrayList<>();
+
+    private Predicate<ToDoItem> excludeSelected = toDoItem -> !toDoItem.equals(selectedToDoItem);
+    private Predicate<ToDoItem> excludeChildren = toDoItem -> !children.contains(toDoItem);
+    private Predicate<ToDoItem> excludeParents = toDoItem -> !parents.contains(toDoItem);
+    private Predicate<String> excludeContexts = string -> !itemContexts.contains(string);
 
     @FXML
     private Label typeLabel;
@@ -50,13 +69,13 @@ public class DialogController {
     @FXML
     private ListView<ToDoItem> depencySourceListView;
     @FXML
-    private ListView<ToDoItem> projectsListView;
+    private ListView<ToDoItem> parentsListView;
     @FXML
-    private Button addProjectButton;
+    private Button addParentButton;
     @FXML
-    private TextField filterProjectSourceTextField;
+    private TextField filterParentSourceTextField;
     @FXML
-    private ListView<ToDoItem> projectSourceListView;
+    private ListView<ToDoItem> parentSourceListView;
     @FXML
     private ListView<String> contextsListView;
     @FXML
@@ -69,6 +88,92 @@ public class DialogController {
     private ListView<String> contextSourceListView;
     @FXML
     private TextArea descriptionTextArea;
+
+    public void initialize() {
+        typeChoiceBox.getItems().add("ToDo");
+        typeChoiceBox.getItems().add("External");
+        typeChoiceBox.getItems().add("Appointment");
+        typeChoiceBox.getItems().add("Project");
+
+        typeChoiceBox.setValue("ToDo");
+        titleTextField.setText("Untitled Item");
+        timeLabel.setVisible(false);
+        timeTextField.setVisible(false);
+        neededLabel.setVisible(false);
+        inheritedLabel.setVisible(false);
+
+        recurringBaseChoiceBox.getItems().add("days");
+        recurringBaseChoiceBox.getItems().add("weeks");
+        recurringBaseChoiceBox.getItems().add("months");
+        recurringBaseChoiceBox.getItems().add("years");
+
+        listViews.add(dependenciesListView);
+        listViews.add(depencySourceListView);
+        listViews.add(parentsListView);
+        listViews.add(parentSourceListView);
+
+        for (ListView<ToDoItem> listView : listViews) {
+            listView.setCellFactory(lv -> new ListCell<ToDoItem>() {
+                @Override
+                protected void updateItem(ToDoItem item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(item.getTitle());
+                    }
+                }
+            });
+        }
+
+        dependenciesListView.setItems(children);
+        parentsListView.setItems(parents);
+        contextsListView.setItems(itemContexts);
+
+        FilteredList<ToDoItem> filteredChildrenSource = new FilteredList<>(toDoItems);
+        FilteredList<ToDoItem> filteredParentSource = new FilteredList<>(toDoItems);
+        FilteredList<String> filteredContextSource = new FilteredList<>(availableContexts);
+
+        depencySourceListView.setItems(filteredChildrenSource);
+        parentSourceListView.setItems(filteredParentSource);
+        contextSourceListView.setItems(filteredContextSource);
+
+        filterDepencySourceTextField.textProperty().addListener(((observable, oldValue, newValue) ->
+                filteredChildrenSource.setPredicate(excludeSelected.and(excludeChildren).and(toDoItem -> {
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+                    return toDoItem.getTitle().toLowerCase().contains(newValue.toLowerCase());
+                }))));
+
+        filterParentSourceTextField.textProperty().addListener(((observable, oldValue, newValue) ->
+                filteredParentSource.setPredicate(excludeSelected.and(excludeParents).and(toDoItem -> {
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+                    return toDoItem.getTitle().toLowerCase().contains(newValue.toLowerCase());
+                }))));
+
+        children.addListener((ListChangeListener<ToDoItem>) c ->
+                filteredChildrenSource.setPredicate(excludeSelected.and(excludeChildren).and(toDoItem -> {
+                    String filter = filterDepencySourceTextField.getText();
+                    if (filter == null || filter.isEmpty()) {
+                        return true;
+                    }
+                    return toDoItem.getTitle().toLowerCase().contains(filter.toLowerCase());
+                })));
+
+        parents.addListener((ListChangeListener<ToDoItem>) c ->
+                filteredParentSource.setPredicate(excludeSelected.and(excludeParents).and(toDoItem -> {
+                    String filter = filterParentSourceTextField.getText();
+                    if (filter == null || filter.isEmpty()) {
+                        return true;
+                    }
+                    return toDoItem.getTitle().toLowerCase().contains(filter.toLowerCase());
+                })));
+
+        itemContexts.addListener((ListChangeListener<String>) c -> filteredContextSource.setPredicate(excludeContexts));
+    }
 
     public void initForm(ToDoItem toDoItem) {
         selectedToDoItem = toDoItem;
@@ -135,14 +240,46 @@ public class DialogController {
             recurrentCheckBox.setSelected(true);
             RecurringPattern recurringPattern = toDoItem.getRecurringPattern();
             everyTextField.setText(((Integer) recurringPattern.getEveryN()).toString());
-            // implement set recurringBase
+            switch (recurringPattern.getRecurringBase()) {
+                case EVERYNDAYS:
+                    recurringBaseChoiceBox.setValue("days");
+                    break;
+                case EVERYNWEEKS:
+                    recurringBaseChoiceBox.setValue("weeks");
+                    break;
+                case EVERYNMONTHS:
+                    recurringBaseChoiceBox.setValue("months");
+                    break;
+                case EVERYNYEARS:
+                    recurringBaseChoiceBox.setValue("years");
+                    break;
+                default:
+                    recurringBaseChoiceBox.setValue("days");
+            }
             if (recurringPattern.isFix()) {
                 fixCheckBox.setSelected(true);
             }
         }
+
+        for (ToDoItem childItem : toDoItem.getDependsOn()) {
+            children.add(childItem);
+        }
+
+        for (ToDoItem parentItem : toDoItem.getDependedOnBy()) {
+            parents.add(parentItem);
+        }
+
+        for (String context : toDoItem.getContexts()) {
+            itemContexts.add(context);
+        }
+
+        String description = toDoItem.getDescription();
+        if (description != null) {
+            descriptionTextArea.setText(description);
+        }
     }
 
     public ToDoItem getToDoItem() {
-        return  null;
+        return null;
     }
 }
