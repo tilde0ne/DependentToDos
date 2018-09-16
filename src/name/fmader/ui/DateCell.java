52a21d286eb9b399d04d9e062c116d36d8759525
tilde0ne <miyako@freenet.de>
2018-09-16 21:38:38 +0200

    DateCell: added color coding for deadlines

--- src/name/fmader/ui/DateCell.java
+++ src/name/fmader/ui/DateCell.java
@@ -1,6 +1,7 @@
 package name.fmader.ui;
 
 import javafx.scene.control.TableCell;
+import javafx.scene.paint.Color;
 import name.fmader.datamodel.ToDoItem;
 
 import java.time.LocalDate;
@@ -13,8 +14,22 @@ public class DateCell extends TableCell<ToDoItem, LocalDate> {
         super.updateItem(item, empty);
         if (item == null || empty) {
             setText(null);
+            setStyle("");
         } else {
             setText(item.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+            if (item.isBefore(LocalDate.now().plusDays(1))) {
+                setStyle("-fx-background-color: red");
+                setTextFill(Color.WHITE);
+            } else if (item.isBefore(LocalDate.now().plusDays(4))) {
+                setStyle("-fx-background-color: orange");
+                setTextFill(Color.BLACK);
+            } else if (item.isBefore(LocalDate.now().plusDays(8))) {
+                setStyle("-fx-background-color: yellow");
+                setTextFill(Color.BLACK);
+            } else {
+                setStyle("-fx-background-color: white");
+                setTextFill(Color.BLACK);
+            }
         }
     }
 }
