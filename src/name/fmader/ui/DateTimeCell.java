5cb9711ff14a7a3a7df151aaf127d4ee3eaadc60
tilde0ne <miyako@freenet.de>
2018-09-17 14:21:08 +0200

    Added color coding dateTimes for appointments

--- src/name/fmader/ui/DateTimeCell.java
+++ src/name/fmader/ui/DateTimeCell.java
@@ -1,8 +1,10 @@
 package name.fmader.ui;
 
 import javafx.scene.control.TableCell;
+import javafx.scene.paint.Color;
 import name.fmader.datamodel.ToDoItem;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -13,8 +15,23 @@ public class DateTimeCell extends TableCell<ToDoItem, LocalDateTime> {
         super.updateItem(item, empty);
         if (item == null || empty) {
             setText(null);
+            setStyle("");
         } else {
             setText(item.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
+            LocalDate date = item.toLocalDate();
+            if (date.isBefore(LocalDate.now().plusDays(1))) {
+                setStyle("-fx-background-color: red");
+                setTextFill(Color.WHITE);
+            } else if (date.isBefore(LocalDate.now().plusDays(4))) {
+                setStyle("-fx-background-color: orange");
+                setTextFill(Color.BLACK);
+            } else if (date.isBefore(LocalDate.now().plusDays(8))) {
+                setStyle("-fx-background-color: yellow");
+                setTextFill(Color.BLACK);
+            } else {
+                setStyle("-fx-background-color: white");
+                setTextFill(Color.BLACK);
+            }
         }
     }
 }
